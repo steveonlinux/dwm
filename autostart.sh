b1d3620e7badcf573ee449643e08750ff8e21108
Stephen Marks <stephenmarks5309@gmail.com>
2022-05-01 01:24:11 -0700

    updated autostart.sh script sourced from ~/.dwm/autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,4 +1,5 @@
 #! /bin/bash 
-#picom --no-fading-openclose &
+#xrandr --output DP-0 --primary --mode 1920x1080 --rate 144.00 --left-of HDMI-0 --output     HDMI-0 --mode 1920x1080 --rate 59.93 &
+picom -c -r 15 --experimental-backends --no-fading-openclose --blur-size 10 --xrender-sync-fence & # --blur-background
 nitrogen --restore &
-#dwmblocks &
+dwmblocks &
